f2e45e6b248470129da0c81ce76f072437976a9c
Edward Wilde <ewilde@gmail.com>
2018-05-18 15:24:01 +0100

    adds pkcs configuration

--- curl-gnutls-opensc/entrypoint.sh
+++ curl-gnutls-opensc/entrypoint.sh
@@ -5,11 +5,8 @@ set -o pipefail
 init(){
 	local pcscd_running=$(ps -aux | grep [p]cscd)
 	if [ -z "$pcscd_running" ]; then
-		echo "starting pcscd in backgroud"
 		pcscd --debug --apdu
 		pcscd --hotplug
-	else
-		echo "pcscd is running in already: ${pcscd_running}"
 	fi
 }
 
